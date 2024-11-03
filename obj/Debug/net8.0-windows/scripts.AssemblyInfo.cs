4900d9c28bcb64a023dab5e36d3dae713c314198
zda9250 <zda9250@rit.edu>
2024-11-03 15:03:32 -0500

    Add in noise files
    Add in noise files for sb

--- obj/Debug/net8.0-windows/scripts.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/scripts.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("storyboard")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84d9770b7e3a62a2a4183160e72fa10893f7652a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8de604c1b49f61611850182c80a001047c955efb")]
 [assembly: System.Reflection.AssemblyProductAttribute("storyboard")]
 [assembly: System.Reflection.AssemblyTitleAttribute("storyboard")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
