fe145a731a53f4c81df774c5c3470bac13e0c9da
zda9250 <zda9250@rit.edu>
2024-11-09 18:23:35 -0500

    Add in new scene and text effects

--- obj/Debug/net8.0-windows/scripts.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/scripts.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("storyboard")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd78a2e469ce37b2087f2495b11bcd6ae279f608")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af8083f9296372f8bdc1b27a456e183bb8670e5b")]
 [assembly: System.Reflection.AssemblyProductAttribute("storyboard")]
 [assembly: System.Reflection.AssemblyTitleAttribute("storyboard")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
