b74770c429ed09a03c9e45679cc4b47b62210121
Himada4 <45938024+Himada4@users.noreply.github.com>
2024-11-10 01:34:59 +0100

    Update Credits.cs

--- Credits.cs
+++ Credits.cs
@@ -64,7 +64,7 @@ namespace StorybrewScripts
             */
             
 
-            generateCredit("Song Composer", "Genkaku Aria (feat. KOKOMI)", 290648, 292494);
+            generateCreditGenkaku("Song Composer", "Genkaku Aria (feat. KOKOMI)", 290648, 292494);
             generateCredit("Mapper", "KKipalt & Icekalt", 292494, 294340);
             generateCredit("Hitsound", "Vermasium", 294340, 296186);
 
@@ -148,29 +148,29 @@ namespace StorybrewScripts
 
         void generateCredit(string title, string name, int start, int end){
             var layer = GetLayer("credits");
+
+            var additiveRADIUS = 30 * 1.5f;
+            var additiveBarLength = 85 * 1.5f;
+            var fontScale = 1.5;
+            var mainSqareScale = 1.5;
         
             var mainSqare = layer.CreateSprite("sb/1px.png");
 
-            var sqarePuddleUpperRight = layer.CreateSprite("sb/1px.png");
-            var sqarePuddleUpperLeft = layer.CreateSprite("sb/1px.png");
-            var sqarePuddleLowerRight = layer.CreateSprite("sb/1px.png");
-            var sqarePuddleLowerLeft = layer.CreateSprite("sb/1px.png");
-
-            var spriteTitle = layer.CreateSprite(fontGenerator.GetTexture(title).Path, OsbOrigin.Centre, new Vector2(320, 240 - 5));
-            var spriteName = layer.CreateSprite(fontGenerator.GetTexture(name).Path, OsbOrigin.Centre, new Vector2(320, 240 + 5));
+            var spriteTitle = layer.CreateSprite(fontGenerator.GetTexture(title).Path, OsbOrigin.Centre, new Vector2(320, 240 - (float)(5 * fontScale)));
+            var spriteName = layer.CreateSprite(fontGenerator.GetTexture(name).Path, OsbOrigin.Centre, new Vector2(320, 240 + (float)(5 * fontScale)));
 
             spriteName.Fade(start, 1);
             spriteName.Fade(end, 0);
-            spriteName.Scale(start, 0.1);
+            spriteName.Scale(start, 0.1 * fontScale);
 
             spriteTitle.Fade(start, 1);
             spriteTitle.Fade(end, 0);
-            spriteTitle.Scale(start, 0.07);
+            spriteTitle.Scale(start, 0.07 * fontScale);
 
-            var additiveUpperRight = layer.CreateSprite("sb/1px.png", OsbOrigin.TopCentre, new Vector2(320 + 30, 240 - 30));
-            var additiveUpperLeft = layer.CreateSprite("sb/1px.png", OsbOrigin.CentreLeft, new Vector2(320 - 30, 240 - 30));
-            var additiveLowerRight = layer.CreateSprite("sb/1px.png", OsbOrigin.CentreRight, new Vector2(320 + 30, 240 + 30));
-            var additiveLowerLeft = layer.CreateSprite("sb/1px.png", OsbOrigin.BottomCentre, new Vector2(320 - 30, 240 + 30));
+            var additiveUpperRight = layer.CreateSprite("sb/1px.png", OsbOrigin.TopCentre, new Vector2(320 + additiveRADIUS, 240 - additiveRADIUS));
+            var additiveUpperLeft = layer.CreateSprite("sb/1px.png", OsbOrigin.CentreLeft, new Vector2(320 - additiveRADIUS, 240 - additiveRADIUS));
+            var additiveLowerRight = layer.CreateSprite("sb/1px.png", OsbOrigin.CentreRight, new Vector2(320 + additiveRADIUS, 240 + additiveRADIUS));
+            var additiveLowerLeft = layer.CreateSprite("sb/1px.png", OsbOrigin.BottomCentre, new Vector2(320 - additiveRADIUS, 240 + additiveRADIUS));
 
             var additives = new List<OsbSprite>(){additiveUpperRight, additiveUpperLeft, additiveLowerRight, additiveLowerLeft};
 
@@ -186,44 +186,113 @@ namespace StorybrewScripts
 
             }
 
-            additiveUpperRight.ScaleVec(OsbEasing.OutCirc, start, start + 500, 85, 85, 85, 0);
-            additiveUpperLeft.ScaleVec(OsbEasing.OutCirc, start, start + 500, 85, 85, 0, 85);
-            additiveLowerRight.ScaleVec(OsbEasing.OutCirc, start, start + 500, 85, 85, 0, 85);
-            additiveLowerLeft.ScaleVec(OsbEasing.OutCirc, start, start + 500, 85, 85, 85, 0);
+            additiveUpperRight.ScaleVec(OsbEasing.OutCirc, start, start + 500, additiveBarLength, additiveBarLength, additiveBarLength, 0);
+            additiveUpperLeft.ScaleVec(OsbEasing.OutCirc, start, start + 500, additiveBarLength, additiveBarLength, 0, additiveBarLength);
+            additiveLowerRight.ScaleVec(OsbEasing.OutCirc, start, start + 500, additiveBarLength, additiveBarLength, 0, additiveBarLength);
+            additiveLowerLeft.ScaleVec(OsbEasing.OutCirc, start, start + 500, additiveBarLength, additiveBarLength, additiveBarLength, 0);
+
+            additiveUpperRight.ScaleVec(OsbEasing.InCirc, end - 100, end, additiveBarLength, 0, additiveBarLength, additiveBarLength);
+            additiveUpperLeft.ScaleVec(OsbEasing.InCirc, end - 100, end, 0, additiveBarLength, additiveBarLength, additiveBarLength);
+            additiveLowerRight.ScaleVec(OsbEasing.InCirc, end - 100, end, 0, additiveBarLength, additiveBarLength, additiveBarLength);
+            additiveLowerLeft.ScaleVec(OsbEasing.InCirc, end - 100, end, additiveBarLength, 0, additiveBarLength, additiveBarLength);
 
 
-            mainSqare.Scale(OsbEasing.OutCirc, start, start + 500, 0, 50);
+            mainSqare.Scale(OsbEasing.OutCirc, start, start + 500, 0, 50 * mainSqareScale);
             mainSqare.Rotate(start, Math.PI/4);
             mainSqare.Color(start, Color4.SkyBlue);
             mainSqare.Fade(end, end, 0.1, 0);
-            mainSqare.Scale(OsbEasing.InCirc, end - 100, end, 50, 0);
+            mainSqare.Scale(OsbEasing.InCirc, end - 100, end, 50 * mainSqareScale, 0);
             
         }
+
+        void generateCreditGenkaku(string title, string name, int start, int end){
+            var layer = GetLayer("credits");
+
+            var additiveRADIUS = 70;
+            var additiveBarLength = 195;
+            var fontScale = 1.5;
+            var mainSqareScale = 1.5;
+        
+            var mainSqare = layer.CreateSprite("sb/1px.png");
+
+
+            var spriteTitle = layer.CreateSprite(fontGenerator.GetTexture(title).Path, OsbOrigin.Centre, new Vector2(320, 240 - (float)(5 * fontScale)));
+            var spriteName = layer.CreateSprite(fontGenerator.GetTexture(name).Path, OsbOrigin.Centre, new Vector2(320, 240 + (float)(5 * fontScale)));
+
+            spriteName.Fade(start, 1);
+            spriteName.Fade(end, 0);
+            spriteName.Scale(start, 0.1 * fontScale);
+
+            spriteTitle.Fade(start, 1);
+            spriteTitle.Fade(end, 0);
+            spriteTitle.Scale(start, 0.07 * fontScale);
+
+            var additiveUpperRight = layer.CreateSprite("sb/1px.png", OsbOrigin.TopCentre, new Vector2(320 + additiveRADIUS, 240 - additiveRADIUS));
+            var additiveUpperLeft = layer.CreateSprite("sb/1px.png", OsbOrigin.CentreLeft, new Vector2(320 - additiveRADIUS, 240 - additiveRADIUS));
+            var additiveLowerRight = layer.CreateSprite("sb/1px.png", OsbOrigin.CentreRight, new Vector2(320 + additiveRADIUS, 240 + additiveRADIUS));
+            var additiveLowerLeft = layer.CreateSprite("sb/1px.png", OsbOrigin.BottomCentre, new Vector2(320 - additiveRADIUS, 240 + additiveRADIUS));
+
+            var additives = new List<OsbSprite>(){additiveUpperRight, additiveUpperLeft, additiveLowerRight, additiveLowerLeft};
+
+
+            foreach(var add in additives){
+
+                add.Fade(start, 1);
+                add.Fade(end, 0);
+                add.Rotate(start, Math.PI/4);
+
+                
+                add.Color(start, Color4.Black);
+
+            }
+
+            additiveUpperRight.ScaleVec(OsbEasing.OutCirc, start, start + 500, additiveBarLength, additiveBarLength, additiveBarLength, 0);
+            additiveUpperLeft.ScaleVec(OsbEasing.OutCirc, start, start + 500, additiveBarLength, additiveBarLength, 0, additiveBarLength);
+            additiveLowerRight.ScaleVec(OsbEasing.OutCirc, start, start + 500, additiveBarLength, additiveBarLength, 0, additiveBarLength);
+            additiveLowerLeft.ScaleVec(OsbEasing.OutCirc, start, start + 500, additiveBarLength, additiveBarLength, additiveBarLength, 0);
+
+            additiveUpperRight.ScaleVec(OsbEasing.InCirc, end - 100, end, additiveBarLength, 0, additiveBarLength, additiveBarLength);
+            additiveUpperLeft.ScaleVec(OsbEasing.InCirc, end - 100, end, 0, additiveBarLength, additiveBarLength, additiveBarLength);
+            additiveLowerRight.ScaleVec(OsbEasing.InCirc, end - 100, end, 0, additiveBarLength, additiveBarLength, additiveBarLength);
+            additiveLowerLeft.ScaleVec(OsbEasing.InCirc, end - 100, end, additiveBarLength, 0, additiveBarLength, additiveBarLength);
+
+
+            mainSqare.Scale(OsbEasing.OutCirc, start, start + 500, 0, 50 * mainSqareScale);
+            mainSqare.Rotate(start, Math.PI/4);
+            mainSqare.Color(start, Color4.SkyBlue);
+            mainSqare.Fade(end, end, 0.1, 0);
+            mainSqare.Scale(OsbEasing.InCirc, end - 100, end, 50 * mainSqareScale, 0);
+            
+        }
+
+
 
         void generateCreditLeft(string title, string name, int start, int end){
             var layer = GetLayer("credits");
 
-            var offset = -300;
+            var Xoffset = -300;
+            var additiveRADIUS = 30 * 1.5f;
+            var additiveBarLength = 85 * 1.5f;
+            var fontScale = 1.5;
+            var mainSqareScale = 1.5;
         
-            var mainSqare = layer.CreateSprite("sb/1px.png", OsbOrigin.Centre, new Vector2(320 + offset, 240));
+            var mainSqare = layer.CreateSprite("sb/1px.png", OsbOrigin.Centre, new Vector2(320 + Xoffset, 240));
 
-            
-
-            var spriteTitle = layer.CreateSprite(fontGenerator.GetTexture(title).Path, OsbOrigin.Centre, new Vector2(320 + offset, 240 - 5));
-            var spriteName = layer.CreateSprite(fontGenerator.GetTexture(name).Path, OsbOrigin.Centre, new Vector2(320 + offset, 240 + 5));
+            var spriteTitle = layer.CreateSprite(fontGenerator.GetTexture(title).Path, OsbOrigin.Centre, new Vector2(320 + Xoffset, 240 - (float)(5 * fontScale)));
+            var spriteName = layer.CreateSprite(fontGenerator.GetTexture(name).Path, OsbOrigin.Centre, new Vector2(320 + Xoffset, 240 + (float)(5 * fontScale)));
 
             spriteName.Fade(start, 1);
             spriteName.Fade(end, 0);
-            spriteName.Scale(start, 0.1);
+            spriteName.Scale(start, 0.1 * fontScale);
 
             spriteTitle.Fade(start, 1);
             spriteTitle.Fade(end, 0);
-            spriteTitle.Scale(start, 0.07);
+            spriteTitle.Scale(start, 0.07 * fontScale);
 
-            var additiveUpperRight = layer.CreateSprite("sb/1px.png", OsbOrigin.TopCentre, new Vector2(320 + 30 + offset, 240 - 30));
-            var additiveUpperLeft = layer.CreateSprite("sb/1px.png", OsbOrigin.CentreLeft, new Vector2(320 - 30 + offset, 240 - 30));
-            var additiveLowerRight = layer.CreateSprite("sb/1px.png", OsbOrigin.CentreRight, new Vector2(320 + 30 + offset, 240 + 30));
-            var additiveLowerLeft = layer.CreateSprite("sb/1px.png", OsbOrigin.BottomCentre, new Vector2(320 - 30 + offset, 240 + 30));
+            var additiveUpperRight = layer.CreateSprite("sb/1px.png", OsbOrigin.TopCentre, new Vector2(320 + additiveRADIUS + Xoffset, 240 - additiveRADIUS));
+            var additiveUpperLeft = layer.CreateSprite("sb/1px.png", OsbOrigin.CentreLeft, new Vector2(320 - additiveRADIUS + Xoffset, 240 - additiveRADIUS));
+            var additiveLowerRight = layer.CreateSprite("sb/1px.png", OsbOrigin.CentreRight, new Vector2(320 + additiveRADIUS + Xoffset, 240 + additiveRADIUS));
+            var additiveLowerLeft = layer.CreateSprite("sb/1px.png", OsbOrigin.BottomCentre, new Vector2(320 - additiveRADIUS + Xoffset, 240 + additiveRADIUS));
 
             var additives = new List<OsbSprite>(){additiveUpperRight, additiveUpperLeft, additiveLowerRight, additiveLowerLeft};
 
@@ -239,44 +308,51 @@ namespace StorybrewScripts
 
             }
 
-            additiveUpperRight.ScaleVec(OsbEasing.OutCirc, start, start + 500, 85, 85, 85, 0);
-            additiveUpperLeft.ScaleVec(OsbEasing.OutCirc, start, start + 500, 85, 85, 0, 85);
-            additiveLowerRight.ScaleVec(OsbEasing.OutCirc, start, start + 500, 85, 85, 0, 85);
-            additiveLowerLeft.ScaleVec(OsbEasing.OutCirc, start, start + 500, 85, 85, 85, 0);
+            additiveUpperRight.ScaleVec(OsbEasing.OutCirc, start, start + 500, additiveBarLength, additiveBarLength, additiveBarLength, 0);
+            additiveUpperLeft.ScaleVec(OsbEasing.OutCirc, start, start + 500, additiveBarLength, additiveBarLength, 0, additiveBarLength);
+            additiveLowerRight.ScaleVec(OsbEasing.OutCirc, start, start + 500, additiveBarLength, additiveBarLength, 0, additiveBarLength);
+            additiveLowerLeft.ScaleVec(OsbEasing.OutCirc, start, start + 500, additiveBarLength, additiveBarLength, additiveBarLength, 0);
+
+            additiveUpperRight.ScaleVec(OsbEasing.InCirc, end - 100, end, additiveBarLength, 0, additiveBarLength, additiveBarLength);
+            additiveUpperLeft.ScaleVec(OsbEasing.InCirc, end - 100, end, 0, additiveBarLength, additiveBarLength, additiveBarLength);
+            additiveLowerRight.ScaleVec(OsbEasing.InCirc, end - 100, end, 0, additiveBarLength, additiveBarLength, additiveBarLength);
+            additiveLowerLeft.ScaleVec(OsbEasing.InCirc, end - 100, end, additiveBarLength, 0, additiveBarLength, additiveBarLength);
 
 
-            mainSqare.Scale(OsbEasing.OutCirc, start, start + 500, 0, 50);
+            mainSqare.Scale(OsbEasing.OutCirc, start, start + 500, 0, 50 * mainSqareScale);
             mainSqare.Rotate(start, Math.PI/4);
             mainSqare.Color(start, Color4.SkyBlue);
             mainSqare.Fade(end, end, 0.1, 0);
-            mainSqare.Scale(OsbEasing.InCirc, end - 100, end, 50, 0);
+            mainSqare.Scale(OsbEasing.InCirc, end - 100, end, 50 * mainSqareScale, 0);
             
         }
 
         void generateCreditRight(string title, string name, int start, int end){
             var layer = GetLayer("credits");
 
-            var offset = 300;
+            var Xoffset = 300;
+            var additiveRADIUS = 30 *  1.5f;
+            var additiveBarLength = 85 * 1.5f;
+            var fontScale = 1.5;
+            var mainSqareScale = 1.5;
         
-            var mainSqare = layer.CreateSprite("sb/1px.png", OsbOrigin.Centre, new Vector2(320 + offset, 240));
+            var mainSqare = layer.CreateSprite("sb/1px.png", OsbOrigin.Centre, new Vector2(320 + Xoffset, 240));
 
-            
-
-            var spriteTitle = layer.CreateSprite(fontGenerator.GetTexture(title).Path, OsbOrigin.Centre, new Vector2(320 + offset, 240 - 5));
-            var spriteName = layer.CreateSprite(fontGenerator.GetTexture(name).Path, OsbOrigin.Centre, new Vector2(320 + offset, 240 + 5));
+            var spriteTitle = layer.CreateSprite(fontGenerator.GetTexture(title).Path, OsbOrigin.Centre, new Vector2(320 + Xoffset, 240 - (float)(5 * fontScale)));
+            var spriteName = layer.CreateSprite(fontGenerator.GetTexture(name).Path, OsbOrigin.Centre, new Vector2(320 + Xoffset, 240 + (float)(5 * fontScale)));
 
             spriteName.Fade(start, 1);
             spriteName.Fade(end, 0);
-            spriteName.Scale(start, 0.1);
+            spriteName.Scale(start, 0.1 * fontScale);
 
             spriteTitle.Fade(start, 1);
             spriteTitle.Fade(end, 0);
-            spriteTitle.Scale(start, 0.07);
+            spriteTitle.Scale(start, 0.07 * fontScale);
 
-            var additiveUpperRight = layer.CreateSprite("sb/1px.png", OsbOrigin.TopCentre, new Vector2(320 + 30 + offset, 240 - 30));
-            var additiveUpperLeft = layer.CreateSprite("sb/1px.png", OsbOrigin.CentreLeft, new Vector2(320 - 30 + offset, 240 - 30));
-            var additiveLowerRight = layer.CreateSprite("sb/1px.png", OsbOrigin.CentreRight, new Vector2(320 + 30 + offset, 240 + 30));
-            var additiveLowerLeft = layer.CreateSprite("sb/1px.png", OsbOrigin.BottomCentre, new Vector2(320 - 30 + offset, 240 + 30));
+            var additiveUpperRight = layer.CreateSprite("sb/1px.png", OsbOrigin.TopCentre, new Vector2(320 + additiveRADIUS + Xoffset, 240 - additiveRADIUS));
+            var additiveUpperLeft = layer.CreateSprite("sb/1px.png", OsbOrigin.CentreLeft, new Vector2(320 - additiveRADIUS + Xoffset, 240 - additiveRADIUS));
+            var additiveLowerRight = layer.CreateSprite("sb/1px.png", OsbOrigin.CentreRight, new Vector2(320 + additiveRADIUS + Xoffset, 240 + additiveRADIUS));
+            var additiveLowerLeft = layer.CreateSprite("sb/1px.png", OsbOrigin.BottomCentre, new Vector2(320 - additiveRADIUS + Xoffset, 240 + additiveRADIUS));
 
             var additives = new List<OsbSprite>(){additiveUpperRight, additiveUpperLeft, additiveLowerRight, additiveLowerLeft};
 
@@ -292,17 +368,23 @@ namespace StorybrewScripts
 
             }
 
-            additiveUpperRight.ScaleVec(OsbEasing.OutCirc, start, start + 500, 85, 85, 85, 0);
-            additiveUpperLeft.ScaleVec(OsbEasing.OutCirc, start, start + 500, 85, 85, 0, 85);
-            additiveLowerRight.ScaleVec(OsbEasing.OutCirc, start, start + 500, 85, 85, 0, 85);
-            additiveLowerLeft.ScaleVec(OsbEasing.OutCirc, start, start + 500, 85, 85, 85, 0);
+            additiveUpperRight.ScaleVec(OsbEasing.OutCirc, start, start + 500, additiveBarLength, additiveBarLength, additiveBarLength, 0);
+            additiveUpperLeft.ScaleVec(OsbEasing.OutCirc, start, start + 500, additiveBarLength, additiveBarLength, 0, additiveBarLength);
+            additiveLowerRight.ScaleVec(OsbEasing.OutCirc, start, start + 500, additiveBarLength, additiveBarLength, 0, additiveBarLength);
+            additiveLowerLeft.ScaleVec(OsbEasing.OutCirc, start, start + 500, additiveBarLength, additiveBarLength, additiveBarLength, 0);
 
 
-            mainSqare.Scale(OsbEasing.OutCirc, start, start + 500, 0, 50);
-            mainSqare.Scale(OsbEasing.InCirc, end - 100, end, 50, 0);
+            additiveUpperRight.ScaleVec(OsbEasing.InCirc, end - 100, end, additiveBarLength, 0, additiveBarLength, additiveBarLength);
+            additiveUpperLeft.ScaleVec(OsbEasing.InCirc, end - 100, end, 0, additiveBarLength, additiveBarLength, additiveBarLength);
+            additiveLowerRight.ScaleVec(OsbEasing.InCirc, end - 100, end, 0, additiveBarLength, additiveBarLength, additiveBarLength);
+            additiveLowerLeft.ScaleVec(OsbEasing.InCirc, end - 100, end, additiveBarLength, 0, additiveBarLength, additiveBarLength);
+
+
+            mainSqare.Scale(OsbEasing.OutCirc, start, start + 500, 0, 50  * mainSqareScale);
             mainSqare.Rotate(start, Math.PI/4);
             mainSqare.Color(start, Color4.SkyBlue);
             mainSqare.Fade(end, end, 0.1, 0);
+            mainSqare.Scale(OsbEasing.InCirc, end - 100, end, 50 * mainSqareScale, 0);
             
         }
     }
